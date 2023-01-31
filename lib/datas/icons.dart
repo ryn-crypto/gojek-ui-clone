2adbb770d928ade083f101fe7c1ecd2bada05aca
ryn-crypto <riyanfirst13@gmail.com>
2023-01-31 20:54:22 +0700

    modeling data icon dan berita

---
+++ lib/datas/icons.dart
@@ -0,0 +1,19 @@
+import 'package:gojek_clone_ui/models/gojek_icon.dart';
+import 'package:gojek_clone_ui/theme.dart';
+
+List<GojekIcon> gopayIcons = [
+  GojekIcon(icon: 'pay', title: 'Bayar'),
+  GojekIcon(icon: 'topup', title: 'Top Up'),
+  GojekIcon(icon: 'explore', title: 'Eksplore')
+];
+
+List<GojekIcon> menuIcons = [
+  GojekIcon(icon: 'goride', title: 'GoRide', color: green1),
+  GojekIcon(icon: 'gocar', title: 'GoCar', color: green2),
+  GojekIcon(icon: 'gofood', title: 'GoFood', color: red),
+  GojekIcon(icon: 'gosend', title: 'GoSend', color: green2),
+  GojekIcon(icon: 'gomart', title: 'GoMart', color: red),
+  GojekIcon(icon: 'gopulsa', title: 'GoPulsa', color: blue2),
+  GojekIcon(icon: 'goclup', title: 'GoClup', color: purple),
+  GojekIcon(icon: 'other', title: 'Lainnya', color: dark4)
+];
