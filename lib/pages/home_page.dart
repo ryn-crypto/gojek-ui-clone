cf642ca4d5b3718df9f8367eb0d27aa1e4bee7ec
ryn-crypto <riyanfirst13@gmail.com>
2023-01-31 21:57:52 +0700

    slicing search bar dan avatar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gojek_clone_ui/components/header.dart';
 import 'package:gojek_clone_ui/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,44 +9,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
           backgroundColor: green2,
           elevation: 0,
           toolbarHeight: 71,
-          title: Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-                color: green1, borderRadius: BorderRadius.circular(30)),
-            child: Row(
-              children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(100)),
-                  child: Text(
-                    'Beranda',
-                    style: semibold14.copyWith(color: green1),
-                  ),
-                ),
-                ...['Promo', 'Pesanan', 'Chat'].map((title) => Flexible(
-                      fit: FlexFit.loose,
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 14, vertical: 12),
-                        child: Center(
-                          child: Text(
-                            title,
-                            style: semibold14,
-                          ),
+          title: const Header()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 23, left: 15, right: 15),
+              child: Row(
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFFAFAFA),
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: const Color(0xFFE8E8E8))),
+                      child: Row(children: [
+                        SvgPicture.asset(
+                          'assets/icons/search.svg',
+                          color: dark1,
+                          width: 20,
+                          height: 20,
                         ),
-                      ),
-                    ))
-              ],
-            ),
-          )),
+                        SizedBox(width: 10),
+                        Text(
+                          'Cari layanan, makanan, & tujuan',
+                          style: regular14.copyWith(color: dark3),
+                        )
+                      ]),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  SizedBox(
+                    width: 35,
+                    height: 35,
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(35 / 2)),
+                          clipBehavior: Clip.hardEdge,
+                          child: Image.asset('assets/images/avatar.png'),
+                        ),
+                        Positioned(
+                            right: 0,
+                            bottom: 0,
+                            child: Container(
+                              width: 16,
+                              height: 16,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(35 / 2),
+                                  color: const Color(0xFFD1E7EE)),
+                              clipBehavior: Clip.hardEdge,
+                              child: SvgPicture.asset(
+                                'assets/icons/goclub.svg',
+                                color: blue3,
+                              ),
+                            ))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
