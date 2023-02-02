2d76037ca8dd2d5abab4e5d7da7518ac19970006
ryn-crypto <riyanfirst13@gmail.com>
2023-02-02 22:16:06 +0700

    menambahkan go club progres

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -120,7 +120,7 @@ class HomePage extends StatelessWidget {
                           height: 11,
                           width: 118,
                           decoration: const BoxDecoration(
-                              color: Color(0xfff9ccddb),
+                              color: Color(0xFF9ccddb),
                               borderRadius: BorderRadius.vertical(
                                   bottom: Radius.circular(8))),
                         ),
@@ -228,6 +228,75 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            // goclub
+            Padding(
+              padding: const EdgeInsets.only(top: 19, left: 15, right: 15),
+              child: Container(
+                height: 65,
+                clipBehavior: Clip.hardEdge,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Color(0xFFEAF3F6), Colors.white]),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color(0xFFE8E8E8),
+                  ),
+                ),
+                child: Stack(children: [
+                  Positioned(
+                      left: 8,
+                      top: 4,
+                      bottom: 4,
+                      child: SvgPicture.asset('assets/icons/dots.svg')),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/star.svg',
+                          height: 40,
+                        ),
+                        const SizedBox(width: 16),
+                        Flexible(
+                          fit: FlexFit.tight,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '117 XP Lagi ada Harta Karun',
+                                style: semibold14.copyWith(color: dark1),
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                width: double.infinity,
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(2)),
+                                child: LinearProgressIndicator(
+                                  backgroundColor: dark3,
+                                  color: green1,
+                                  value: .8,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 24),
+                        SvgPicture.asset(
+                          'assets/icons/left.svg',
+                          color: dark1,
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+              ),
+            )
           ],
         ),
       ),
