e7d4c150d7056eb91d50fdf5fec2ad302b11a539
ryn-crypto <riyanfirst13@gmail.com>
2023-02-03 21:06:54 +0700

    menambahkan quick access

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gojek_clone_ui/components/header.dart';
@@ -18,6 +19,7 @@ class HomePage extends StatelessWidget {
           title: const Header()),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Search
             Padding(
@@ -241,9 +243,7 @@ class HomePage extends StatelessWidget {
                       end: Alignment.centerRight,
                       colors: [Color(0xFFEAF3F6), Colors.white]),
                   borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: const Color(0xFFE8E8E8),
-                  ),
+                  border: Border.all(color: const Color(0xFFE8E8E8)),
                 ),
                 child: Stack(children: [
                   Positioned(
@@ -295,6 +295,65 @@ class HomePage extends StatelessWidget {
                     ),
                   )
                 ]),
+              ),
+            ),
+            // Quick Access
+            Padding(
+              padding: const EdgeInsets.only(left: 15, top: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Akses Cepat',
+                    style: bold18.copyWith(color: dark1),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: const Color(0xFFE8E8E8))),
+                    child: Column(
+                      children: [
+                        ...[
+                          'Pintu masuk motor, MNC Land',
+                          'Pintu keluar motor, MNC Land'
+                        ].map((text) => Padding(
+                              padding: const EdgeInsets.all(16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 24,
+                                    height: 24,
+                                    decoration: BoxDecoration(
+                                        color: green2,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/goride.svg',
+                                      color: Colors.white,
+                                      width: 24,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 12),
+                                  Flexible(
+                                    fit: FlexFit.tight,
+                                    child: Text(
+                                      text,
+                                      style: regular14.copyWith(color: dark1),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 24),
+                                  SvgPicture.asset(
+                                    'assets/icons/left.svg',
+                                    color: dark1,
+                                  )
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                  )
+                ],
               ),
             )
           ],
